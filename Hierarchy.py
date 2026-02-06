61a20647671a71dc872f2f774fa4f6ab4a3af047
ZeeliA <rosalizee@gmail.com>
2026-02-06 15:52:48 +0100

    Syntax update

--- Hierarchy.py
+++ Hierarchy.py
@@ -1,3 +1,16 @@
+#--------------------------------------------------------------#
+#                     ZR Hierarchy                             #
+#                     Author : ZeeliA                          #
+#                     v.2026-02-06-001                         #
+#--------------------------------------------------------------#
+
+import maya.cmds as cmds
+
+#---------------------------------------------------------------
+# Function : Creating a hierarchy for a new rig
+#---------------------------------------------------------------
+
+
 def Hierarchy() :
 
 	import maya.cmds as cmds
@@ -25,15 +38,15 @@ def Hierarchy() :
 		cmds.error("Nothing entered")
 		
 
-	charaGrp = cmds.group(em=True, name = assetName)
-	geoGrp = cmds.group(em = True, name="GEO", parent = charaGrp)
-	curvesGrp = cmds.group(em = True, name = "CURVES", parent = charaGrp)
-	skinGrp = cmds.group(em = True, name = "SKELETON")
-	rigGrp = cmds.group(em = True, name = "RIGGING")
-	ctlGrp = cmds.group(em = True, name = "CONTROLLERS")
+	chara_grp = cmds.group(em=True, name = assetName)
+	geo_grp = cmds.group(em = True, name="GEO", parent = chara_grp)
+	curves_grp = cmds.group(em = True, name = "CURVES", parent = chara_grp)
+	skin_grp = cmds.group(em = True, name = "SKELETON")
+	rig_grp = cmds.group(em = True, name = "RIGGING")
+	ctl_grp = cmds.group(em = True, name = "CONTROLLERS")
 
-	cmds.parent(sel, geoGrp)
-	sizeValue = cmds.xform(geoGrp, query = 1, bb = 1)
+	cmds.parent(sel, geo_grp)
+	sizeValue = cmds.xform(geo_grp, query = 1, bb = 1)
 	sizeY = sizeValue[4]-sizeValue[1]
 
 
@@ -41,10 +54,10 @@ def Hierarchy() :
 	cmds.addAttr(mainCtl[0], ln="GEO_SIZE", attributeType="float", defaultValue = sizeY )
 
 
-	cmds.parent(mainCtl[0], charaGrp)
-	cmds.parent(skinGrp, mainCtl[0])
-	cmds.parent(rigGrp, mainCtl[0])
-	cmds.parent(ctlGrp, mainCtl[0])
+	cmds.parent(mainCtl[0], chara_grp)
+	cmds.parent(skin_grp, mainCtl[0])
+	cmds.parent(rig_grp, mainCtl[0])
+	cmds.parent(ctl_grp, mainCtl[0])
 
 	cmds.addAttr(mainCtl[0], k = True, sn='skn', ln='SKELETON_VISIBILITY', defaultValue=1, minValue=0, maxValue=1 )
 	cmds.addAttr(mainCtl[0], k = True, sn='rig', ln='RIGGING_VISIBILITY', defaultValue=1, minValue=0, maxValue=1 )
@@ -52,11 +65,11 @@ def Hierarchy() :
 	cmds.addAttr(mainCtl[0], k = True, sn='mshlock', ln='MESH_LOCK', defaultValue=1, minValue=0, maxValue=1 )
 	cmds.addAttr(mainCtl[0], k = True, sn='hop', ln='HIDE_ON_PLAYBACK', defaultValue=1, minValue=0, maxValue=1 )
 
-	cmds.connectAttr(mainCtl[0] + ".skn", skinGrp +".visibility")
-	cmds.connectAttr(mainCtl[0] + ".rig", rigGrp +".visibility")
-	cmds.connectAttr(mainCtl[0] + ".ctlVis", ctlGrp +".visibility")
-	cmds.connectAttr(mainCtl[0] + ".mshlock", geoGrp +".overrideEnabled")
-	cmds.connectAttr(mainCtl[0] + ".hop", ctlGrp +".hideOnPlayback")
+	cmds.connectAttr(f"{mainCtl[0]}.skn", f"{skin_grp}.visibility")
+	cmds.connectAttr(f"{mainCtl[0]}.rig", f"{rig_grp}.visibility")
+	cmds.connectAttr(f"{mainCtl[0]}.ctlVis", f"{ctl_grp}.visibility")
+	cmds.connectAttr(f"{mainCtl[0]}.mshlock", f"{geo_grp}.overrideEnabled")
+	cmds.connectAttr(f"{mainCtl[0]}.hop", f"{ctl_grp}.hideOnPlayback")
 
-	cmds.setAttr(mainCtl[0] + ".overrideEnabled", 1 )
-	cmds.setAttr(mainCtl[0] + ".overrideColor", 17 )
+	cmds.setAttr(f"{mainCtl[0]}.overrideEnabled", 1 )
+	cmds.setAttr(f"{mainCtl[0]}.overrideColor", 17 )
